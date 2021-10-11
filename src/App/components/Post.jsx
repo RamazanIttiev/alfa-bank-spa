b3a5fc4dac680a46141dad9a14d472fc888bc029
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-10-11 22:09:30 +0300

    favorites icon refactoring

--- src/App/components/Post.jsx
+++ src/App/components/Post.jsx
@@ -8,27 +8,17 @@ import {
   Avatar,
   IconButton,
   Typography,
+  Checkbox,
 } from '@mui/material';
 
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
-import Heart from '../../assets/Heart.svg';
+import { Favorite, FavoriteBorder } from '@mui/icons-material';
 import { removeFavourites, addFavourites } from '../store/favouritesSlice';
 
 const Base = styled(Card)`
   max-width: 345px;
   border-radius: 16px;
-`;
-
-const Favorite = styled.img`
-  filter: ${props =>
-    props.colored
-      ? 'invert(98%) sepia(50%) saturate(1141%) hue-rotate(318deg) brightness(106%) contrast(105%);'
-      : 'invert(100%) sepia(0%) saturate(0%) hue-rotate(67deg) brightness(113%) contrast(101%);'}
-
-  &:hover {
-    filter: invert(98%) sepia(50%) saturate(1141%) hue-rotate(318deg) brightness(106%) contrast(105%);
-  }
 `;
 
 const Post = ({ id, image, owner, text, publishDate, isFavourite }) => {
@@ -52,7 +42,10 @@ const Post = ({ id, image, owner, text, publishDate, isFavourite }) => {
       </CardContent>
       <CardActions disableSpacing>
         <IconButton onClick={handleClick}>
-          <Favorite src={Heart} colored={isFavourite} alt="Add to favourites" />
+          <Checkbox
+            icon={<FavoriteBorder style={{ color: '#fff' }} />}
+            checkedIcon={<Favorite />}
+          />
         </IconButton>
       </CardActions>
     </Base>
--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,9 @@ import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
   palette: {
-    main: '#FFF59D',
+    primary: {
+      main: '#FFF59D',
+    },
     type: 'dark',
     background: {
       default: '#191A1C',
