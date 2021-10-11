20f97e5ac2357838f4451b301515cd702d30a19b
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-10-11 11:51:46 +0300

    configured store

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
 import App from './App/App';
 import { theme } from './theme';
+import { store } from './App/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
