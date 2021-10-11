22b682925bb191af83d97d1b3b6be09e6af8ea70
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-10-11 10:10:01 +0300

    added material UI components to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import App from './App/App';
+import { theme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
