4eed546caca16434e519d2f780600fa6d1e683e9
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-10-11 10:08:31 +0300

    theme configurations

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from '@mui/material';
+
+export const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    background: {
+      default: '#191A1C',
+      paper: '#2f3237',
+    },
+    text: {
+      primary: '#ffffff',
+      secondary: '#00000',
+    },
+  },
+});
